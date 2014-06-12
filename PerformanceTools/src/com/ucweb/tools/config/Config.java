b2c4cb193a7a84d29580f807f33f131787915c00
JohnnyAWK <289008087@qq.com>
2014-06-12 11:43:02 +0800

    Update Config.java

--- PerformanceTools/src/com/ucweb/tools/config/Config.java
+++ PerformanceTools/src/com/ucweb/tools/config/Config.java
@@ -2,15 +2,15 @@ package com.ucweb.tools.config;
 
 public class Config {
 	/**上传URL*/
-	public static final String UPLOAD_URL = "http://115.238.230.18:28043/Analysis/UploadServlet";
+	public static final String UPLOAD_URL = "";
 	
-	public static final String UPDATE_URL = "http://an.test2.game.uc.cn:28043/Analysis/uploadsApp/CheckVersion.jsp";
+	public static final String UPDATE_URL = "";
 	
 	/**线程池大小*/
 	public static final int MAX_THREAD_POOL_SIZE = 3;
 	
 	/**获取Monkey脚本URL*/
-	public static final String GET_MONKEY_SCRIPT_URL = "http://an.test2.game.uc.cn:28043/Analysis/MonkeyServlet";
+	public static final String GET_MONKEY_SCRIPT_URL = "";
 	
 	/**Monkey脚本名字*/
 	public static final String MONKEY_SCRIPT_FILE_NAME = "monkey_script.txt";
